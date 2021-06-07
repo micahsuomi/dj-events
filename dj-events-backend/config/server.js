a638522bbda85f301f4c54e9dbf5ae990a6495b4
Michele Zucca <michele.zucca81@gmail.com>
2021-06-07 08:45:46 +0300

    added map boilerplate

---
+++ dj-events-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b76b836e59cfd49805dbd1f4a32cd248'),
+    },
+  },
+});
---
+++ dj-events-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bfb5b909-cce7-4ab6-825e-44c2b2c3e309'
+};
